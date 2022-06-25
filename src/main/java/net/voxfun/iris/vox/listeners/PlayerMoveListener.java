625899ffdacd4f5048682d94572b33e235f69749
Ella <thenightcraftyt@gmail.com>
2022-06-25 04:47:54 -0400

    t

--- src/main/java/net/voxfun/iris/vox/listeners/PlayerMoveListener.java
+++ src/main/java/net/voxfun/iris/vox/listeners/PlayerMoveListener.java
@@ -1,9 +1,12 @@
 package net.voxfun.iris.vox.listeners;
 
 import net.voxfun.iris.vox.index;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,12 +14,29 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMoveListener implements Listener{
     @EventHandler
-    public void onPlyerWalk(PlayerMoveEvent event) {
+    public void onPlayerWalk(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         if (index.isMainLobby) {
 
             Location blockUnder = player.getLocation();
             blockUnder.setY(blockUnder.getY() - 1);
+
+            //Changing blocks
+
+            final BlockState state = blockUnder.getBlock().getState();
+
+            int delay = 20;
+
+            //Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            //    @Override
+            //    public void run() {
+            //        state.update(true);
+            //    }
+            //}, delay);
+
+
+            //Launch Pads
+
             if (player.getLocation().getBlock().getType().equals((Material.LIGHT_WEIGHTED_PRESSURE_PLATE)) && blockUnder.getBlock().getType().equals(Material.JACK_O_LANTERN)) {
                 player.setVelocity(player.getLocation().getDirection().multiply(3).setY(0.7));
                 player.playSound(player.getLocation(), Sound.ENTITY_WOLF_SHAKE, 10, 1);
