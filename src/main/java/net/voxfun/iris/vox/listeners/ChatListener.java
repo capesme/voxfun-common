02abf8762fac7879d81519552d361989ed3ec1c3
Ella <thenightcraftyt@gmail.com>
2022-06-28 01:29:42 -0400

    Updated Chatting

--- src/main/java/net/voxfun/iris/vox/listeners/ChatListener.java
+++ src/main/java/net/voxfun/iris/vox/listeners/ChatListener.java
@@ -1,15 +1,21 @@
 package net.voxfun.iris.vox.listeners;
 
+import net.voxfun.iris.vox.index;
 import net.voxfun.iris.vox.managers.PlayerManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
     @EventHandler
-    public static void onChat(AsyncPlayerChatEvent e) {
-        e.setCancelled(true);
-        Bukkit.broadcastMessage(String.format("%s%s: %s", PlayerManager.getRank(e.getPlayer().getUniqueId().toString()), e.getPlayer().getName(), e.getMessage()));
+    public static void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        event.setFormat(PlayerManager.getRank(player.getUniqueId().toString()) + "" + ChatColor.WHITE + "" + player.getName() + ": " + message);
+        event.setMessage(message);
     }
 }
--- src/main/java/net/voxfun/iris/vox/listeners/PlayerActionListener.java
+++ src/main/java/net/voxfun/iris/vox/listeners/PlayerActionListener.java
@@ -8,10 +8,8 @@
         import org.bukkit.event.EventHandler;
         import org.bukkit.event.Listener;
         import org.bukkit.event.inventory.InventoryClickEvent;
-        import org.bukkit.event.inventory.InventoryInteractEvent;
         import org.bukkit.event.player.PlayerDropItemEvent;
         import org.bukkit.event.player.PlayerInteractEvent;
-        import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerActionListener implements Listener{
 
